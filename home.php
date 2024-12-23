8aaac0b16d962647ae8db1b83ade461d07c3e081
Connor Geller <98245605+CG2312@users.noreply.github.com>
2024-12-22 20:50:57 -0500

    Basic landing page

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<title>Welcome to my Webpage!</title>
+
+<body>
+    <p>This webpage is written in php and html</p>
+    <?php
+    echo "My first PHP script!";
+    ?>
+</body>
+
+</html>
