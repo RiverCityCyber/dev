63bde365f86f8d841a01fd2baa8ce8db80f292c8
Connor <98245605+CG2312@users.noreply.github.com>
2025-06-19 20:21:20 -0400

    Updated css theme light mode to tan Light mode button persists across pages

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 /* Dark Mode JS */
 document.addEventListener('DOMContentLoaded', function () {
     const toggle = document.getElementById('dm-toggle');
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark").matches;
-    if (prefersDark) {
-        document.body.classList.add('dark-mode');
-        toggle.textContext = 'Dark Mode';
-    } else {
-        document.body.classList.add('light-mode');
-        document.body.classList.remove('dark-mode');
-    }
-    toggle.addEventListener('click', function () {
-        if (document.body.classList.contains('dark-mode')) {
-            document.body.classList.remove('dark-mode');
-            document.body.classList.add('light-mode');
-            toggle.textContent = 'Dark Mode';
-        } else {
-            document.body.classList.remove('light-mode');
+    let mode = localStorage.mode || (window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light');
+
+    function applyMode(selectedMode) {
+        if (selectedMode === 'dark') {
             document.body.classList.add('dark-mode');
+            document.body.classList.remove('light-mode');
             toggle.textContent = 'Light Mode';
+        } else {
+            document.body.classList.add('light-mode');
+            document.body.classList.remove('dark-mode');
+            toggle.textContent = 'Dark Mode';
         }
-    })
+        mode = selectedMode;
+        localStorage.mode = selectedMode;
+    }
+
+    applyMode(mode);
+
+    toggle.addEventListener('click', function () {
+        const newMode = document.body.classList.contains('dark-mode') ? 'light' : 'dark';
+        applyMode(newMode);
+    });
 });
 /* End Dark Mode JS */
